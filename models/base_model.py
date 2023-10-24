a4ae72f2e0bf5e02613ac9aeb8dd777e8752f67a
MateoZouko <134229522+MateoZouko@users.noreply.github.com>
2023-10-24 12:46:41 -0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,32 @@
+#!/usr/bin/python3
 import uuid
 from datetime import datetime
+"""Creating a base model that defines common attributes"""
+
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
+    """defining the class"""
+    def __init__(self, otherId=None):
+        """defining the init of the class Base Model"""
+        if otherId is not None:
+            self.id = otherId
+        else:
+            self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-    def __str__(self):
-        class_name = self.__class__.__name__
-        return f"[{class_name}] ({self.id}) {self.__dict__}"
+    def save(self):
+        """Defining save public instance method"""
+        self.updated_at = datetime.now()
 
-# Example usage:
-if __name__ == "__main__":
-    base_model = BaseModel()
-    print(base_model)
+    def to_dict(self):
+        """Defining to dict public instance method"""
+        instanceDict = self.__dict__.copy()
+        instanceDict['__class__'] = self.__class__.__name__
+        instanceDict['updated_at'] = self.updated_at.isoformat()
+        instanceDict['created_at'] = self.created_at.isoformat()
+        return instanceDict
+
+    def __str__(self):
+        """Defining __str__"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
