3413fec9befebbe35921276bfb4fbec0460fa9d4
juandi131 <juandigarcia3@gmail.com>
2023-10-25 16:26:58 -0700

    12

--- console.py
+++ console.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
         """Quit the program"""
         return True
-    
+
     def do_EOF(self, arg):
         """Exit the program using Ctrl+D"""
         return True
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
