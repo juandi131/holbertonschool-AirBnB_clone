c7e4257c8d0ec45dc9920356db93fcbbf8151ab7
Mateo Zouko <mateozouko1@gmail.com>
2023-10-25 12:55:39 +0000

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,6 @@ class BaseModel:
     def save(self):
         """Defining save public instance method"""
         self.updated_at = datetime.now()
-<<<<<<< HEAD
 
     def to_dict(self):
         """Defining to dict public instance method"""
@@ -31,25 +30,3 @@ class BaseModel:
     def __str__(self):
         """Defining __str__"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-# Crear una instancia de la clase BaseModel
-lol = BaseModel()
-
-# Llamar al método to_dict en la instancia
-diccionario = lol.to_dict()
-
-# Imprimir el diccionario
-print(diccionario)
-=======
-
-    def to_dict(self):
-        """Defining to dict public instance method"""
-        instanceDict = self.__dict__.copy()
-        instanceDict['__class__'] = self.__class__.__name__
-        instanceDict['updated_at'] = self.updated_at.isoformat()
-        instanceDict['created_at'] = self.created_at.isoformat()
-        return instanceDict
-
-    def __str__(self):
-        """Defining __str__"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
->>>>>>> a4ae72f2e0bf5e02613ac9aeb8dd777e8752f67a
