2ff207763abc811fb7684c6bdfdddba71a05989d
juandi131 <juandigarcia3@gmail.com>
2023-10-25 14:02:18 -0700

    consola

--- consola.py
+++ consola.py
@@ -5,9 +5,20 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
+        """Exit the program"""
         return True
+    
     def do_EOF(self, arg):
         """Exit the program using Ctrl+D (EOF)"""
         return True
+    def do_create(self, arg):
+        """
+        Create a new object.
+        
+        Usage: create <class_name>
+        Example: create User
+        """
+        pass
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
