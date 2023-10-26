37209eef82244b08ea7f00f6ff9af0b90b8cfb8c
Mateo Zouko <mateozouko1@gmail.com>
2023-10-26 16:37:44 +0000

    fixing task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import json
+from models.base_model import BaseModel
+from models.user import User
 """Writing a class FileStorage that serializes 
 nstances to a JSON file and deserializes JSON
 file to instances:"""
@@ -7,10 +9,9 @@ file to instances:"""
 
 class FileStorage():
     """creating the class FileStorage"""
-    def __init__(self):
-        """Initializing the class"""
-        self.__file_path = "file.json"
-        self.__objects = {}
+    __file_path = "file.json"
+    __objects = {}
+    objectClass = {"BaseModel": BaseModel, "User": User}
         
     def all(self):
         """ returns the dictionary __objects"""
@@ -24,7 +25,7 @@ class FileStorage():
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         variable = {}
-        for key,value in self.__objects.items():
+        for key, value in self.__objects.items():
             variable[key] = value.to_dict()
         with open(self.__file_path, 'w') as file:
             json.dump(variable, file)
@@ -34,11 +35,10 @@ class FileStorage():
         (__file_path) exists;otherwise, do nothing.
         If the file doesn’t exist, no exception should be raised)"""
         try:
-            with open(self.__file_path, 'r', encoding='utf-8') as file:
-                cont = json.load(file)
-                for key, value in cont.items():
-                    class_name = value["__class__"]
-                    obj = globals()[class_name](**value)
+            with open(self.__file_path, 'r') as file:
+                inside = json.load(file)
+                for key, value in inside.items():
+                    obj = self.objectClas[value["__class__"]](**value)
                     self.__objects[key] = obj
-        except FileNotFoundError:
+        except Exception:
             pass
